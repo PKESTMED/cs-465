3c0109361e69500ac4f6b9ccfc54a61dfc144801
Pedro <estrada.pach@gmail.com>
2023-03-06 17:59:40 -0600

    use dynamic view rendering

--- app.js
+++ app.js
@@ -3,14 +3,20 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const hbs = require('hbs');
 
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
+var travelRouter = require('./app_server/routes/travel');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'app_server', 'views'));
+
+  // register handlebars partials (hhtps://www.npmjs.com/package/hbs)
+  hbs.registerPartials(path.join(__dirname, 'app_server', 'views/partials'))
+
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
@@ -21,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/travel', travelRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
