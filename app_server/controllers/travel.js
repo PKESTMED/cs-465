9017e74276cdc30972224e34b0d8a3166ad3ba1b
Pedro <estrada.pach@gmail.com>
2023-03-15 16:16:03 -0500

    json

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,6 +1,9 @@
+var fs = require('fs');
+var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf-8'));
+
 const travel = (req, res) => {
     pageTitle = process.env.npm_package_description + ' - Travel';
-    res.render('travel', { title: pageTitle });
+    res.render('travel', { title: pageTitle, trips });
 };
 
 module.exports = {
