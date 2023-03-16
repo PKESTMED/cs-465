d9fd786a40bf3ee2160348ddb54955c2f87f220e
Pedro <estrada.pach@gmail.com>
2023-03-16 15:06:23 -0500

    Convert template from static html to JSON

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
-var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf-8'));
+var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
 
 const travel = (req, res) => {
     pageTitle = process.env.npm_package_description + ' - Travel';
